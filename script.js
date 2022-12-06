d311d231e57622ab4b39c028aa8f978ff0d90f1a
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2022-12-06 10:37:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ function draw() {
   translate(width / 2, height / 2);
   scale(5);
   let mm = map(mX, 0, width, -1, 1);
-  let my = map(mY, 0, height, -1, 1);
+  let my = map(mY, 0, height, -100, 0);
 
   for (let a = 0; a < 2.5; a += 0.0004) {
     stroke(255)
     strokeWeight(0.151)
     let x = a*10000;
-    let y = (a)/tan(x+zoff)+mm;
-    let e = (cos(x))*80;
-    circle(sin(tan(cos(y))+mm)*e, sin(tan(sin(y))+mm)*e,0.03);
+    let y = noise(a)/tan(x+zoff)*my;
+    let e = (cos(x))*width/20;
+    //circle(cos(((x*e))-my)*e, atan(((y/a))/mm)*e,0.13);
+    circle(cos(((e*a))-my)*e, atan(((y/e))/mm)*e,0.13);
+
   }
   zoff += 0.035;
 }
